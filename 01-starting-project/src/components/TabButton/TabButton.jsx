01f606edfdba2d6c6aa9ce974a681a4f6a63bfe4
wjinss <aastu4343@gmail.com>
2025-08-19 03:43:39 +0900

    learn : 49. 이벤트 처리

--- 01-starting-project/src/components/TabButton/TabButton.jsx
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,7 +1,10 @@
 export default function TabButton({ children, label }) {
+  function handleClick() {
+    console.log("안녕! 세상아!");
+  }
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
       {/* <button>{label}</button> */}
     </li>
   );
