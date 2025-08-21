d3d84607c057948aebbc0719c8132a458dec99fb
wjinss <aastu4343@gmail.com>
2025-08-21 12:47:17 +0900

    learn : 이벤트 핸들러에 커스텀 인자 전달하기

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,8 +5,9 @@ import ConreConept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
-  function handleSelect() {
-    console.log("안녕! 세상아! - 선택됨!");
+  function handleSelect(selectedButton) {
+    // selectedButton => '컴포넌트'. "jsx", "props", "state"
+    console.log(`안녕! 세상아! - ${selectedButton}!`);
   }
   return (
     <div>
@@ -25,10 +26,14 @@ function App() {
           <h2>Examples</h2>
           <menu>
             {/* <TabButton label={"레이블"} /> */}
-            <TabButton onSelect={handleSelect}>컴포넌트</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>상태</TabButton>
+
+            {/* 이벤트 핸들러에 인자를 함께 전달하고 싶을 때 익명 화살표함수로 만든다. 전달만 할때는 함수 자체를  전달, 실행x  */}
+            <TabButton onSelect={() => handleSelect("컴포넌트")}>
+              컴포넌트
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("프롭스")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("상태")}>상태</TabButton>
           </menu>
           동적 콘텐츠
         </section>
