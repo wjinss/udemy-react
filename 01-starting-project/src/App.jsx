12de37d14605cf5bdc4c46b843a0cb9ad2a19158
wjinss <aastu4343@gmail.com>
2025-08-21 15:04:30 +0900

    learn : 56. css동적 스타일링

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -46,48 +46,32 @@ function App() {
             {/* <TabButton label={"레이블"} /> */}
 
             {/* 이벤트 핸들러에 인자를 함께 전달하고 싶을 때 익명 화살표함수로 만든다. 전달만 할때는 함수 자체를  전달, 실행x  */}
-            <TabButton onSelect={() => handleSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
               Components
             </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
 
-          {/* 방법 1 */}
-          {!selectedTopic ? (
-            <p>주제를 골라주세요!</p>
-          ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-          {/* 방법2 */}
-          {!selectedTopic ? <p>주제를 골라주세요!</p> : null}
-          {selectedTopic ? (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          ) : null}
-          {/* 방법3 */}
-          {!selectedTopic && <p>주제를 골라주세요!</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
           {/* 방법4 */}
           {tabContent}
         </section>
@@ -97,3 +81,56 @@ function App() {
 }
 
 export default App;
+
+() => {
+  {
+    /* 방법 1 */
+  }
+  {
+    !selectedTopic ? (
+      <p>주제를 골라주세요!</p>
+    ) : (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+  {
+    /* 방법2 */
+  }
+  {
+    !selectedTopic ? <p>주제를 골라주세요!</p> : null;
+  }
+  {
+    selectedTopic ? (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    ) : null;
+  }
+  {
+    /* 방법3 */
+  }
+  {
+    !selectedTopic && <p>주제를 골라주세요!</p>;
+  }
+  {
+    selectedTopic && (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+};
--- 01-starting-project/src/components/TabButton/TabButton.jsx
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,8 +1,10 @@
-export default function TabButton({ children, label, onSelect }) {
+export default function TabButton({ children, label, onSelect, isSelected }) {
   console.log("버튼 컴포넌트 렌더링");
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
       {/* <button>{label}</button> */}
     </li>
   );
