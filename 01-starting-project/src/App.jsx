d665df8e3a375fcb7b6012b9853d18fbedbd3575
wjinss <aastu4343@gmail.com>
2025-08-22 16:07:38 +0900

    learn. 60~61. React.createElement, Fragments

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -75,7 +75,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
