95b6afbb203b6d883785ac25dd9ff90e2b1ec7ae
wjinss <aastu4343@gmail.com>
2025-09-02 02:09:02 +0900

    learn. ~77 이전 값에 기반한 상태를 변경할땐 상태 변경 함수에 값을 전달하지 않고 업데이트 함수를 전달한다

--- 02-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,24 +4,27 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   };
   const inputID = useId();
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = (
+      <>
+        <label htmlFor={inputID}></label>
+        <input type="text" required id={inputID} value={name} />
+      </>
+    );
+  }
   return (
     <li>
       <span className="player">
-        {isEditing ? (
-          <>
-            <label htmlFor={inputID}></label>
-            <input type="text" required id={inputID} />
-          </>
-        ) : (
-          <span className="player-name">{name}</span>
-        )}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button type="button" onClick={handleEditClick}>
-        Edit
+        {isEditing ? "Save" : "Edit"}
       </button>
     </li>
   );
