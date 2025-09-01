80cfc39aaed91d8e6e25961c4e2d5d06bc2d63f5
wjinss <aastu4343@gmail.com>
2025-09-02 03:05:25 +0900

    learn. ~80 상태 불변으로 업데이트

--- 02-tic-tac-toe-starting-project/src/App.jsx
+++ 02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
-import Player from "./components/Player";
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           <Player initialName={"player 1"} symbol={"✅"} />
           <Player initialName={"player 2"} symbol={"❌"} />
         </ol>
-        Game Board
+        <GameBoard />
       </div>
     </main>
   );
