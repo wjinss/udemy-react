5115a0aba91a65528bd457acabbdaaf96eeb8e72
wjinss <aastu4343@gmail.com>
2025-11-15 22:36:32 +0900

    learn. app컴포넌트 필사

--- 03-Investment-Calculator/src/App.jsx
+++ 03-Investment-Calculator/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     duration: 10,
   });
 
+  // 인풋값에 적절한 값이 들어오는지 확인(년도수가 0보다 적어지지 않게)
   const inputIsValid = userInput.duration >= 1;
 
   // 새로운 값이 들어가면 이전 값이 복사되고, 매개변수로 받은 식별자에 새로운 값을 할당
@@ -35,3 +36,37 @@ function App() {
 }
 
 export default App;
+
+//--------------------------------------------------------
+
+(function App() {
+  // 초기 투자금, 연간 투자량, 예상 수익, 투자 기간의 상태를 객체로 관리(초기값을 설정한다~)
+  const [userInput2, setUserInput2] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  // 인풋값에 적절한 값이 들어오는지 확인(년도수가 0보다 적어지지 않게)
+  const inputIsValid2 = userInput2.duration >= 1;
+
+  // 새로운 값이 들어가면 이전 값이 복사되고, 매개변수로 받은 식별자에 새로운 값을 할당
+  function handleChange2(inputIdentifier2, newValue2) {
+    setUserInput2((prevUserInput2) => {
+      return { ...prevUserInput2, [inputIdentifier2]: +newValue2 }; // +를 붙이는 건 문자열 값을 숫자 값으로 변환하는 것을 강제함
+    });
+  }
+
+  return (
+    <>
+      <Header></Header>
+      <UserInput onChange={handleChange2} userInput2={userInput2} />
+      {inputIsValid2 ? (
+        <Results input={userInput2} />
+      ) : (
+        <p className="center">0보다 큰 기간을 입력해주세요!</p>
+      )}
+    </>
+  );
+});
