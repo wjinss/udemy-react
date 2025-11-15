074bd7b6dfb7cf8fcbad87a8b89531212f66c835
wjinss <aastu4343@gmail.com>
2025-11-15 23:08:57 +0900

    learn. userInput 컴포넌트 필사

--- 03-Investment-Calculator/src/components/UserInput.jsx
+++ 03-Investment-Calculator/src/components/UserInput.jsx
@@ -44,3 +44,51 @@ export default function UserInput({ onChange, userInput }) {
     </section>
   );
 }
+
+//------------------------------------
+(function UserInput({ onChange, userInput }) {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="">초기 투자액</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(e) => onChange("initialInvestment", e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="">연간 투자량</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(e) => onChange("annualInvestment", e.target.value)}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="">예상 수익</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(e) => onChange("expectedReturn", e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="">투자 기간</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(e) => onChange("duration", e.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+});
