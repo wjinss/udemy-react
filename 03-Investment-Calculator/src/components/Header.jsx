5309287b5f1836129113ea56d30c05016ae069eb
wjinss <aastu4343@gmail.com>
2025-11-15 22:39:21 +0900

    learn. header컴포넌트 필사

--- 03-Investment-Calculator/src/components/Header.jsx
+++ 03-Investment-Calculator/src/components/Header.jsx
@@ -8,3 +8,14 @@ export default function Header() {
     </header>
   );
 }
+
+//--------------------------------------------------------
+
+(function Header() {
+  return (
+    <header id="header">
+      <img src={logo} alt="돈가방이 보이는 로고" />
+      <h1>투자금 계산기</h1>
+    </header>
+  );
+});
