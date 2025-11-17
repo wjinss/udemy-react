1fb9b21d9edd20f253ee4cc4db9070c79bfa9024
wjinss <aastu4343@gmail.com>
2025-11-17 16:00:32 +0900

    learn. ~107 / css파일 분리, 퓨어css의 단점: 스코핑이 되지 않기에 특정 컴포넌트에서 호출이 돼도 다른 컴포넌트에 중복될 가능성이 있다.

--- 04-styling/src/components/Header.jsx
+++ 04-styling/src/components/Header.jsx
@@ -1,4 +1,5 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import "./Header.css";
 
 export default function Header() {
   return (
@@ -7,5 +8,6 @@ export default function Header() {
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
+    // 퓨어css는 스코핑이 되지 않기에, 헤더 컴포넌트의 스타일이 다른 컴포넌트에 적용될 가능성이 있다.
   );
 }
