4080038fe9078bfd0c97abcf54bf960ccef772de
wjinss <aastu4343@gmail.com>
2025-11-17 17:08:49 +0900

    learn. ~108 / 인라인 스타일, 장점: 동적 스타일이 간단함,  단점: jsx모든 요소에 적용해야 돼서 비효율적

--- 04-styling/src/components/Header.jsx
+++ 04-styling/src/components/Header.jsx
@@ -6,6 +6,13 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
+      <p
+        style={{
+          color: "red",
+        }}
+      >
+        A community of artists and art-lovers.
+      </p>
       <p>A community of artists and art-lovers.</p>
     </header>
     // 퓨어css는 스코핑이 되지 않기에, 헤더 컴포넌트의 스타일이 다른 컴포넌트에 적용될 가능성이 있다.
