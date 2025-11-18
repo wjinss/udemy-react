127badcd88170a0e2c8300a787b5e9bd563f6a44
wjinss <aastu4343@gmail.com>
2025-11-18 13:56:55 +0900

    learn. ~110 조건부 동적 스타일링(클래스)

--- 04-styling/src/components/Header.jsx
+++ 04-styling/src/components/Header.jsx
@@ -13,7 +13,6 @@ export default function Header() {
       >
         A community of artists and art-lovers.
       </p>
-      <p>A community of artists and art-lovers.</p>
     </header>
     // 퓨어css는 스코핑이 되지 않기에, 헤더 컴포넌트의 스타일이 다른 컴포넌트에 적용될 가능성이 있다.
   );
