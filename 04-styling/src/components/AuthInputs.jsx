8461f7e9ba4b58efbd9dee5f8a86d8068d3ace4c
wjinss <aastu4343@gmail.com>
2025-11-19 15:15:07 +0900

    learn. ~122. 테일윈드의 장점: - 빠른 속도, 중복 이슈 x, 개인 설절 및 커스텀 기능 단점: - 클래스명이 길어짐, jsx코드와 스타일 코드의 분리가 명확하지 않음

--- 04-styling/src/components/AuthInputs.jsx
+++ 04-styling/src/components/AuthInputs.jsx
@@ -1,24 +1,6 @@
 import { useState } from "react";
-import { styled } from "styled-components";
-
 import SignButton from "./SignButton";
 import Input from "./Input";
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
-const CreateAccountButton = styled.button`
-  color: #f0b322;
-  border: none;
-
-  &:hover {
-    color: #f0920e;
-  }
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -41,32 +23,17 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-linear-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           label="Email"
           type="email"
           invalid={emailNotValid}
           onChange={(event) => handleInputChange("email", event.target.value)}
         ></Input>
-        {/* <p>
-          <Label
-            $invalid={emailNotValid} // props 명명규칙(대부분 $를 붙임)
-            // className={`label ${emailNotValid ? "invalid" : ""}`}
-          >
-            Email
-          </Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
-            // style={{
-            //   backgroundColor: emailNotValid ? "#fed2d2" : "#did5db",
-            // }}
-            // className={emailNotValid ? "invalid" : undefined}
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
-        </p> */}
-
         <Input
           label="Password"
           type="password"
@@ -75,37 +42,16 @@ export default function AuthInputs() {
             handleInputChange("password", event.target.value)
           }
         />
-        {/* <p>
-          <Input
-            invalid={passwordNotValid}
-            // className={`label ${emailNotValid ? "invalid" : ""}`}
-          >
-            Password
-          </Input>
-          <Input
-            type="password"
-            // className={passwordNotValid ? "invalid" : undefined}
-            invalid={passwordNotValid}
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </p> */}
-      </ControlContainer>
-      <div className="actions">
-        <CreateAccountButton
-          type="button"
-          // className="text-button"
-        >
+      </div>
+      <div className="flex justify-end gap-4 ">
+        <button type="button" className="text-amber-400 hover:bg-amber-500">
           Create a new account
-        </CreateAccountButton>
-        <SignButton
-          // className="button"
-          onClick={handleLogin}
-        >
-          Sign In
-        </SignButton>
+        </button>
+        <SignButton onClick={handleLogin}>Sign In</SignButton>
       </div>
     </div>
   );
 }
+
+// 테일윈드 장점: 빠른 속도, 중복 이슈 x, 개인설정 및 커스텀 가능
+// 단점: 클래스가 길어짐, jsx코드와 스타일코드의 분리가 명확하지 않음,
