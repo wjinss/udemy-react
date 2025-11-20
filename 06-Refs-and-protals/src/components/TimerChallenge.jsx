14f1cbc273af4281fd86a3fef492fe763530e85c
wjinss <aastu4343@gmail.com>
2025-11-20 17:59:34 +0900

    learn. ~136 TimerChallenge / 상태에 따른 조건부 렌더링

--- 06-Refs-and-protals/src/components/TimerChallenge.jsx
+++ 06-Refs-and-protals/src/components/TimerChallenge.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handelStop() {}
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>졌어용</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button type="button">챌린지 시작</button>
+        <button type="button" onClick={handleStart}>
+          챌린지 {timerStarted ? "멈춰!" : "시작"}
+        </button>
       </p>
-      <p className="">째깍째깍..</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "째깍째깍" : "..."}
+      </p>
     </section>
   );
 }
