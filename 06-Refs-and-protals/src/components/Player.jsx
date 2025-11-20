7f2bea9e0b890be132c40d5c541361ec33f94c33
wjinss <aastu4343@gmail.com>
2025-11-20 17:09:57 +0900

    learn. ~132 state를 ref로 변경하기

--- 06-Refs-and-protals/src/components/Player.jsx
+++ 06-Refs-and-protals/src/components/Player.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const playerName = useRef();
   const [enteredPlayerName, setEnteredPlaterName] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
 
-  function handelChange(e) {
-    setSubmitted(false);
-    setEnteredPlaterName(e.target.value);
-  }
+  // function handelChange(e) {
+  //   setSubmitted(false);
+  //   setEnteredPlaterName(e.target.value);
+  // }
 
   function handelClick() {
-    setSubmitted(true);
+    setEnteredPlaterName(playerName.current.value);
+    // useRef로 받는 참조는 항상 js객체이며, current로 접근해야된다.
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handelChange} value={enteredPlayerName} />
+        <input
+          type="text"
+          ref={playerName}
+          // onChange={handelChange}
+          // value={enteredPlayerName}
+        />
         <button onClick={handelClick}>Set Name</button>
       </p>
     </section>
