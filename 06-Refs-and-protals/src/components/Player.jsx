66d3950e371f800f6f4b64859b2a6967360be295
wjinss <aastu4343@gmail.com>
2025-11-20 16:57:32 +0900

    learn. ~131 ref전 상태 관리하기 복습

--- 06-Refs-and-protals/src/components/Player.jsx
+++ 06-Refs-and-protals/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlaterName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handelChange(e) {
+    setSubmitted(false);
+    setEnteredPlaterName(e.target.value);
+  }
+
+  function handelClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handelChange} value={enteredPlayerName} />
+        <button onClick={handelClick}>Set Name</button>
       </p>
     </section>
   );
