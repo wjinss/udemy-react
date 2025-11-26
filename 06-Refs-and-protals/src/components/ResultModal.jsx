23e4309abb1212a4667e911b33e69fde27124548
wjinss <aastu4343@gmail.com>
2025-11-26 15:22:04 +0900

    learn. TimerChallenger, ResultModal 컴포넌트 주석 설명 및 필사

--- 06-Refs-and-protals/src/components/ResultModal.jsx
+++ 06-Refs-and-protals/src/components/ResultModal.jsx
@@ -68,3 +68,53 @@ export default function ResultModal({
   // forwardRef를 사용하면 export default로 내보내야함
   // export default ForwardResultModal
 };
+
+// 최신화
+// ----------------------------------------
+(function ResultModal3({ ref, targetTime, remainingTime, onReset }) {
+  const dialog3 = useRef();
+
+  const userLost2 = remainingTime <= 0; // 남은 시간이 0이거나 0보다 작을때 유저가 진 상태
+  const formattedRemainingTime2 = (remainingTime / 1000).toFixed(2);
+  // 남은 시간을 소수점으로 나타내는 로직 / 남은 시간을 1000으로 나눠 소수점으로 표시하고, 앞의 2자리까지 표기
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+  // 점수 계산 로직: 남은 시간에서 타겟타임(초 단위)을 나눈 값을 1에서 빼고, 100을 곱한 후(백분율) 반올림처리
+  // ex) 1 - (남은 시간 0.5초 / 타겟 타임 1초) => 0.5
+  // 0.5 * 100 = 50 (백분율)
+  // 점수는 50점
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog3.current.showModal();
+      },
+    };
+  });
+  // 컴포넌트 외부에서 접근할 수 있는 프로퍼티와 메서드를 정의할 수 있음
+  // 첫 번째 인수로 ref객체, 두 번째 인수로 함수를 받는다.
+  // 인수로 받는 함수는 컴포넌트롤 통해 접근할 수 있는 다른 컴포넌트에 노출돼야 하는 모든 프로퍼티와 메서드를 그룹화하는 객체를 반환
+  // 즉 하위 컴포넌트가 어떻게 바뀌든 상위 컴포넌트에선 open()만 사용해 모달을 오픈할 수 있게 만듦 > 관리는 하위컴포넌트에서
+  // 남용 x
+
+  return createPortal(
+    // 포탈을 사용해 모달 컴포넌트의 html위치(구조적 흐름)을 변경
+    <dialog ref={dialog3} className="result-modal" onClose={onReset}>
+      {userLost2 && <h2>졌습니다!</h2>}
+      {/* 유저가 진 상태일때 조건부 렌더링 */}
+      {!userLost2 && <h2>당신의 점수는 {score}</h2>}
+      {/* 유저가 이긴 상태일때 조건부 렌더링 */}
+      <p>
+        시간 : <strong>{targetTime}초</strong>
+      </p>
+      <p>
+        <strong>{formattedRemainingTime2}</strong>를 남기고 멈췄어요!
+      </p>
+      <form method="dialog" onClose={onReset}>
+        {/* 빌트인 기능인 method="dialog"로 모달 구현 및 esc로 모달이 닫히게 onClose에 onReset연결  */}
+        <button>닫기</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+    // createPortal의 첫 번째 인수는 jsx, 두 번째 인수는 위치할 dom요소
+  );
+});
--- 06-Refs-and-protals/src/components/TimerChallenge.jsx
+++ 06-Refs-and-protals/src/components/TimerChallenge.jsx
@@ -98,3 +98,67 @@ export default function TimerChallenge({ title, targetTime }) {
 //     </section>
 //   );
 // });
+
+// 최신화
+// ---------------------------------------------
+(function TimerChallenge3({ title, targetTime }) {
+  const timer3 = useRef(); // setInterval의 id를 받을 수 있게 ref로 설정
+  const dialog3 = useRef(); // 모달 컴포넌트를 참조하기 위해 ref로 설정
+
+  const [timeRemaining2, setTimeRemaining2] = useState(targetTime * 1000);
+  // 남은 시간을 상태로 관리 / targetTime * 1000는 밀리초를 초로 계산
+
+  const timerIsActive2 =
+    timeRemaining2 > 0 && timeRemaining2 < targetTime * 1000;
+  // 타이머가 작동 중인지 확인하는 함수
+  // 남은 시간이 0보다 크고, 타겟시간(초)보다 작으면 타이머가 작동 중이다~
+
+  if (timeRemaining2 <= 0) {
+    clearInterval(timer3.current);
+    dialog3.current.open();
+  } // 남은 시간이 0보다 작을 때, 즉 시간이 다 흘렀을때 처리
+  // 타이머(셋인터벌) 중단, 모달창 오픈
+
+  function handleReset3() {
+    setTimeRemaining2(targetTime * 1000);
+  } // 리셋 함수: 남은 시간을 초기화(초 기준의 타겟타임으로 설정)
+
+  function handleStart3() {
+    timer3.current = setInterval(() => {
+      setTimeRemaining2((prevTimeRemaining2) => prevTimeRemaining2 - 10);
+    }, 10);
+  } // 타이머 함수 : 0.01초(10밀리초) 마다 반복되며, 남은 시간 상태를 이전 시간에서 0.01초마다 뺀 값으로 최신화한다(즉 5 > 4.99 > 4.98 > 4.97 ...)
+
+  function handleStop3() {
+    dialog3.current.open();
+    clearInterval(timer3.current);
+  } // 정지 함수 : 모달을 열며, 타이머를 멈춤
+
+  return (
+    <>
+      <ResultModal
+        ref={dialog3}
+        targetTime={targetTime}
+        remainingTime={timeRemaining2}
+        onReset={handleReset3}
+        // 모달 참조, 타겟타임, 남은 시간, 리셋함수를 하위 컴포넌트로 전달
+        // 참조를 하위 컴포넌트로 props로 전달하는 건 리액트19부터 가능하며, 이전 버전에선 forwardRef를 사용해 넘긴다
+      />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second {targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerIsActive2 ? handleStop3 : handleStart3}>
+            {/* 타이머가 작동 중이면 정지 함수를, 작동 전이면 타이머 함수(시작) 실행 */}
+            챌린지 {timerIsActive2 ? "멈춰!" : "시작"}
+          </button>
+        </p>
+        <p className={timerIsActive2 ? "active" : undefined}>
+          {timerIsActive2 ? "째깍째깍" : "..."}
+        </p>
+      </section>
+    </>
+  );
+});
