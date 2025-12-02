bb696459717370b61de88ca8d9e888a1d4041fbf
wjinss <aastu4343@gmail.com>
2025-12-02 23:48:04 +0900

    learn. ~147 사이드바 컴포넌트 제작

---
+++ 07-Practice-Project-Project-Management/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <h2>내 프로젝트</h2>
+      <div>
+        <button>+ 프로젝트 추가</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
