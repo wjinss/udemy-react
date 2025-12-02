d86417b1cdb49ffd1fb941bfd10e5b2bb9ea06b2
wjinss <aastu4343@gmail.com>
2025-12-02 23:53:56 +0900

    learn. ~148 사이드바  컴포넌트 스타일링

--- 07-Practice-Project-Project-Management/src/App.jsx
+++ 07-Practice-Project-Project-Management/src/App.jsx
@@ -2,7 +2,7 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8">
       <Sidebar />
     </main>
   );
--- 07-Practice-Project-Project-Management/src/components/Sidebar.jsx
+++ 07-Practice-Project-Project-Management/src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
 export default function Sidebar() {
   return (
-    <aside>
-      <h2>내 프로젝트</h2>
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        내 프로젝트
+      </h2>
       <div>
-        <button>+ 프로젝트 추가</button>
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+          + 프로젝트 추가
+        </button>
       </div>
       <ul></ul>
     </aside>
