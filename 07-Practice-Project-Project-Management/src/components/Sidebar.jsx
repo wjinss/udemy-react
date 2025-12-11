c1c0b27e616d94d108b5b1fa41eb492f2b7e5920
wjinss <aastu4343@gmail.com>
2025-12-11 23:22:42 +0900

    learn. 섹션9 Sidebar컴포넌트 필사

--- 07-Practice-Project-Project-Management/src/components/Sidebar.jsx
+++ 07-Practice-Project-Project-Management/src/components/Sidebar.jsx
@@ -16,3 +16,23 @@ export default function Sidebar({ onStartAddProject }) {
     </aside>
   );
 }
+
+// ------------------------------------------------------------------------------
+
+() => {
+  function Sidebar({ onStartAddProject }) {
+    return (
+      <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+        <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+          내 프로젝트
+        </h2>
+        <div>
+          <ProjectButton onClick={onStartAddProject}>
+            + 프로젝트 추가
+          </ProjectButton>
+        </div>
+        <ul></ul>
+      </aside>
+    );
+  }
+};
