3c43997b2083fdd970bf4d688c8955d320fd8c4b
wjinss <aastu4343@gmail.com>
2025-12-11 23:32:45 +0900

    learn. 섹션9 NoProjectSelected컴포넌트 필사

--- 07-Practice-Project-Project-Management/src/components/NoProjectSelected.jsx
+++ 07-Practice-Project-Project-Management/src/components/NoProjectSelected.jsx
@@ -24,3 +24,30 @@ export default function NoProjectSelected({ onStartAddProject }) {
     </div>
   );
 }
+
+// ------------------------------------------------------------------------------
+
+() => {
+  function NoProjectSelected({ onStartAddProject }) {
+    return (
+      <div className="mt-24 text-center w-2/3">
+        <img
+          src={noProjectImage}
+          alt="빈 할 일 목록"
+          className="w-16 h-16 object-contain mx-auto"
+        />
+        <h2 className="text-xl font-bold text-stone-500 my-4">
+          프로젝트가 선택되지 않았어요
+        </h2>
+        <p className="text-stone-400 mb-4">
+          프로젝트를 선택하거나 새로 만들어보세요!
+        </p>
+        <p className="mt-8">
+          <ProjectButton onClick={onStartAddProject}>
+            새 프로젝트 생성
+          </ProjectButton>
+        </p>
+      </div>
+    );
+  }
+};
