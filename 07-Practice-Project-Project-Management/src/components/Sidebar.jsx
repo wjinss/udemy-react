7dcd31c6c7999215911ccfe25ccd49cd769c7b4f
wjinss <aastu4343@gmail.com>
2025-12-14 22:28:59 +0900

    learn. 섹션9 Sidebar 재필사

--- 07-Practice-Project-Project-Management/src/components/Sidebar.jsx
+++ 07-Practice-Project-Project-Management/src/components/Sidebar.jsx
@@ -45,7 +45,12 @@ export default function Sidebar({
 // ------------------------------------------------------------------------------
 
 () => {
-  function Sidebar({ onStartAddProject }) {
+  function Sidebar({
+    onStartAddProject,
+    projects,
+    onSeletProject,
+    selectedProjectId,
+  }) {
     return (
       <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
         <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -53,10 +58,28 @@ export default function Sidebar({
         </h2>
         <div>
           <ProjectButton onClick={onStartAddProject}>
-            + 프로젝트 추가
+            프로젝트 추가
           </ProjectButton>
         </div>
-        <ul></ul>
+        <ul className="mt-8">
+          {projects.map((project) => {
+            let cssClasses =
+              "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800";
+            if (project.id === selectedProjectId) {
+              cssClasses += " bg-stone-800 text-stone-200";
+            } else {
+              cssClasses += " text-stone-400";
+            }
+            <li key={project.id}>
+              <button
+                className={cssClasses}
+                onClick={() => onSeletProject(project.id)}
+              >
+                {project.title}
+              </button>
+            </li>;
+          })}
+        </ul>
       </aside>
     );
   }
