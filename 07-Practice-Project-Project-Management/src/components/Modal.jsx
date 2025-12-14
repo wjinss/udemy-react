05f70ffcc0b713c23ff5f0100c2e8196d32d841c
wjinss <aastu4343@gmail.com>
2025-12-14 22:44:59 +0900

    learn. 섹션9 Modal컴포넌트 필사

--- 07-Practice-Project-Project-Management/src/components/Modal.jsx
+++ 07-Practice-Project-Project-Management/src/components/Modal.jsx
@@ -30,3 +30,35 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
 });
 
 export default Modal;
+
+// ------------------------------------------------------------------------------
+
+() => {
+  const Modal = forwardRef(function Modal({ children, buttonCaption }) {
+    const dialog = useRef();
+
+    // useImperativeHandle함수를 사용해 open()이라는 함수를 정의(상위 컴포넌트에선 open만 사용해도 모달이 작동되게 명시)
+    useImperativeHandle(ref, () => {
+      return {
+        open() {
+          dialog.current.showModal();
+        },
+      };
+    });
+
+    // 포탈을 사용해서 dom의 위치를 조정
+    return createPortal(
+      <dialog
+        ref={dialog}
+        className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+      >
+        {children}
+        <form method="dialog">
+          <ProjectButton>{buttonCaption}</ProjectButton>
+        </form>
+      </dialog>,
+      document.getElementById("modal-root")
+      // createPortal의 두 번째 인수로 dom의 위치를 선택(적용할 html요소)
+    );
+  });
+};
