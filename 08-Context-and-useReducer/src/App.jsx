879c6dabd77d28df9dbe838d96c2402305b49e4c
wjinss <aastu4343@gmail.com>
2025-12-16 23:52:29 +0900

    learn. ~172 컨텍스트 외부로 분리 및 분리된 콘텍스트에 상태 부여

--- 08-Context-and-useReducer/src/App.jsx
+++ 08-Context-and-useReducer/src/App.jsx
@@ -1,81 +1,14 @@
-import { useState } from "react";
-
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import { CartContext } from "./store/shopping-cart-context.jsx";
+import CartContextProvider from "./store/shopping-cart-context.jsx";
 
 function App() {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [], // 값을 배열로 설정할 수 있지만 지금처럼 명시된 값으로 사용하는 것이 더 동적으로 사용할 수 있다
-  });
-
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  const ctxValue = {
-    items: shoppingCart.items,
-    addItemToCart: handleAddItemToCart,
-    updateCartItemQuantity: handleUpdateCartItemQuantity,
-  };
-
   return (
     // .Provider를 붙이는건 리액트 18까지만이고 19부턴 생략해도 된다
-    <CartContext.Provider value={ctxValue}>
+    <CartContextProvider>
+      {/* <CartContext.Provider value={ctxValue}> */}
       {/* Context.Provider에는 value를 꼭 명시해야된다!! */}
       {/* Context를 사용할 때는 일반적으로 Provider가 제공하는 값을 쓰지만, 어떤 컴포넌트가 Context값을 요청했을 땐 
       해당 컴포넌트에 유효한 Provider가 없으면 그 컴포넌트는 Context를 처음 만들 때 지정했던 기본값을 사용한다. */}
@@ -89,7 +22,8 @@ function App() {
           </li>
         ))}
       </Shop>
-    </CartContext.Provider>
+      {/* </CartContext.Provider> */}
+    </CartContextProvider>
   );
 }
 
--- 08-Context-and-useReducer/src/store/shopping-cart-context.jsx
+++ 08-Context-and-useReducer/src/store/shopping-cart-context.jsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { DUMMY_PRODUCTS } from "../dummy-products.js";
 
 export const CartContext = createContext({
   items: [],
@@ -9,3 +10,75 @@ export const CartContext = createContext({
 // Context를 사용할 때는 일반적으로 Provider가 제공하는 값을 쓰지만, 어떤 컴포넌트가 Context값을 요청했을 땐
 // 해당 컴포넌트에 유효한 Provider가 없으면 그 컴포넌트는 Context를 처음 만들 때 지정했던 기본값을 사용한다.
 // 또한 기본값을 설정하면 자동완성이 되기에 DX가 더 좋다
+
+export default function CartContextProvider({ children }) {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [], // 값을 배열로 설정할 수 있지만 지금처럼 명시된 값으로 사용하는 것이 더 동적으로 사용할 수 있다
+  });
+
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+    updateCartItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
